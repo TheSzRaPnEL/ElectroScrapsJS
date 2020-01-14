8584de66757e1ad37179296480033e3a249b09e2
Mariusz SzRaPnEL Zaborski <szrapnel@gmail.com>
2020-01-14 22:03:11 +0100

    logos added to menu screen, new electrosystem logo

--- MenuLevel.js
+++ MenuLevel.js
@@ -30,10 +30,47 @@ class MenuLevel extends PIXI.Sprite {
 			startBtn.off('pointerdown', onButtonDown);
 			context.stop(context);
 		}
+		
+		context.awardIcon = new PIXI.Sprite(PIXI.Texture.from("EnergyGlobeLogo.png"));
+		var awardIcon=context.awardIcon;
+			awardIcon.visible=false;
+			awardIcon.buttonMode = true;
+			awardIcon.anchor.set(0.5);
+			awardIcon.x = 105;
+			awardIcon.y = 95;
+		context.addChild(awardIcon);
+		
+		context.electroSystemLogo = new PIXI.Sprite(PIXI.Texture.from("ElectroSystemLogo2020.png"));
+		var electroSystemLogo=context.electroSystemLogo;
+			electroSystemLogo.visible=false;
+			electroSystemLogo.buttonMode = true;
+			electroSystemLogo.x = app.renderer.width-electroSystemLogo.width;
+			electroSystemLogo.y = app.renderer.height;
+		context.addChild(electroSystemLogo);
+		
+		context.chlorofilLogo = new PIXI.Sprite(PIXI.Texture.from("ChlorofilLogo.png"));
+		var chlorofilLogo=context.chlorofilLogo;
+			chlorofilLogo.visible=false;
+			chlorofilLogo.buttonMode = true;
+			chlorofilLogo.anchor.set(0.5);
+			chlorofilLogo.x = 105;
+			chlorofilLogo.y = app.renderer.height;
+		context.addChild(chlorofilLogo);
 	}
 	
 	begin(context) {
 		context.startBtn.visible=true;
+		
+		context.awardIcon.visible=true;
+		context.awardIcon.scale.x=2;
+		context.awardIcon.scale.y=2;
+		gsap.to(context.awardIcon.scale,2,{x:1,y:1, ease:Elastic.easeOut});
+		
+		context.electroSystemLogo.visible=true;
+		gsap.to(context.electroSystemLogo,2,{y:app.renderer.height-context.electroSystemLogo.height});
+		
+		context.chlorofilLogo.visible=true;
+		gsap.to(context.chlorofilLogo,2,{y:app.renderer.height-40, ease:Elastic.easeOut});
 	}
 	
 	stop(context) {
